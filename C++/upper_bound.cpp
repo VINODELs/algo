2ca3aff31fbb1c161a099bedabd8dbbc9c942b63
Denis Stafilov <stafilov38@mail.ru>
2022-01-11 18:22:56 +0300

    added more algorithms

--- C++/upper_bound.cpp
+++ C++/upper_bound.cpp
@@ -3,13 +3,13 @@
 #include <vector>
 
 template <typename RandomAccessIter, typename ValueType>
-RandomAccessIter LowerBound(RandomAccessIter first, RandomAccessIter last, ValueType target) {
+RandomAccessIter UpperBound(RandomAccessIter first, RandomAccessIter last, ValueType target) {
     RandomAccessIter left = first;
     RandomAccessIter right = last;
     
     while (std::distance(left, right) > 0) {
         RandomAccessIter middle = left + std::distance(left, right) / 2;
-        if (*middle < target) {
+        if (*middle <= target) {
             left = middle + 1;
         } else {
             right = middle;
@@ -20,7 +20,7 @@ RandomAccessIter LowerBound(RandomAccessIter first, RandomAccessIter last, Value
 
 int main() {
     std::vector<int> s{1, 1, 1, 1, 1, 2, 2, 2, 2, 5, 5, 5, 5};
-    auto iter = std::lower_bound(s.begin(), s.end(), 2);
-    auto iter_2 = LowerBound(s.begin(), s.end(), 2);
+    auto iter = std::upper_bound(s.begin(), s.end(), 5);
+    auto iter_2 = UpperBound(s.begin(), s.end(), 5);
     std::cout << (iter == iter_2) << "\n";
 }
